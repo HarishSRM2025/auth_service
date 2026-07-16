5dd2e244210c3e68bc467d76575cf11db916208f
HarishSRM2025 <harishsrm25@gmail.com>
2026-07-16 15:02:06 +0530

    latest

--- sequelize.js
+++ sequelize.js
@@ -1,60 +1,60 @@
-// const { Sequelize } = require("sequelize");
-
-// const sequelize = new Sequelize("rydmate", "root", "2526", {
-//     host: "localhost",
-//     dialect: "mysql"
-// });
-
-// sequelize.authenticate()
-//     .then(() => console.log("MySQL Connected"))
-//     .catch(err => console.log("DB Error:", err));
-
-// module.exports = sequelize;
-
-
-
-require("dotenv").config();
 const { Sequelize } = require("sequelize");
 
-const connectionUrl = process.env.DB_URL;
+const sequelize = new Sequelize("erp_db", "root", "2526", {
+    host: "localhost",
+    dialect: "mysql"
+});
 
-const commonOptions = {
-    dialect: "postgres",
-    logging: false,
-};
-
-const renderSslOptions = {
-    dialectOptions: {
-        ssl: {
-            require: true,
-            rejectUnauthorized: false,
-        },
-    },
-};
-
-const shouldUseSsl = process.env.DB_SSL === "true" || Boolean(connectionUrl);
-
-const sequelize = connectionUrl
-    ? new Sequelize(connectionUrl, {
-        ...commonOptions,
-        ...(shouldUseSsl ? renderSslOptions : {}),
-    })
-    : new Sequelize(
-        process.env.DB_NAME,
-        process.env.DB_USER,
-        process.env.DB_PASS,
-        {
-            ...commonOptions,
-            host: process.env.DB_HOST,
-            port: process.env.DB_PORT || 5432,
-            ...(shouldUseSsl ? renderSslOptions : {}),
-        }
-    );
-
-if (!process.env.VERCEL) {
-    sequelize.authenticate()
-        .then(() => console.log("PostgreSQL Connected"))
-        .catch(err => console.log("DB Error:", err));
-}
+sequelize.authenticate()
+    .then(() => console.log("MySQL Connected"))
+    .catch(err => console.log("DB Error:", err));
 
 module.exports = sequelize;
+
+
+
+// require("dotenv").config();
+// const { Sequelize } = require("sequelize");
+
+// const connectionUrl = process.env.DB_URL;
+
+// const commonOptions = {
+//     dialect: "postgres",
+//     logging: false,
+// };
+
+// const renderSslOptions = {
+//     dialectOptions: {
+//         ssl: {
+//             require: true,
+//             rejectUnauthorized: false,
+//         },
+//     },
+// };
+
+// const shouldUseSsl = process.env.DB_SSL === "true" || Boolean(connectionUrl);
+
+// const sequelize = connectionUrl
+//     ? new Sequelize(connectionUrl, {
+//         ...commonOptions,
+//         ...(shouldUseSsl ? renderSslOptions : {}),
+//     })
+//     : new Sequelize(
+//         process.env.DB_NAME,
+//         process.env.DB_USER,
+//         process.env.DB_PASS,
+//         {
+//             ...commonOptions,
+//             host: process.env.DB_HOST,
+//             port: process.env.DB_PORT || 5432,
+//             ...(shouldUseSsl ? renderSslOptions : {}),
+//         }
+//     );
+
+// if (!process.env.VERCEL) {
+//     sequelize.authenticate()
+//         .then(() => console.log("PostgreSQL Connected"))
+//         .catch(err => console.log("DB Error:", err));
+// }
+
+// module.exports = sequelize;
